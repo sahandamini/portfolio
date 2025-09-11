4a06ad9377bbf5acc9bedcb2ae47cf1681b77318
Sahand Amini <sahand775@gmail.com>
2025-09-11 19:47:32 -0400

    Undo threshold

--- e2e/index.spec.ts
+++ e2e/index.spec.ts
@@ -6,27 +6,27 @@ test.beforeEach(async ({ page }) => {
 
 test('Basic Navigation', async ({ page }) => {
 	await expect(page.locator('h1')).toHaveText('Sahand Amini')
-	
+
 	// Home page
 	await expect(page).toHaveScreenshot('home.png', {
-		maxDiffPixelRatio: 0.02,
+		maxDiffPixelRatio: 0.1,
 	})
 
 	// Scroll to main details
 	await page.getByRole('button').filter({ hasText: /^$/ }).first().click()
 	await expect(page).toHaveScreenshot('about-me.png', {
-		maxDiffPixelRatio: 0.02,
+		maxDiffPixelRatio: 0.1,
 	})
 
 	// Scroll to bottom
 	await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight))
 	await expect(page).toHaveScreenshot('bottom.png', {
-		maxDiffPixelRatio: 0.02,
+		maxDiffPixelRatio: 0.1,
 	})
 
 	// Scroll back to top
 	await page.getByRole('button', { name: 'Scroll to top' }).click()
 	await expect(page).toHaveScreenshot('home.png', {
-		maxDiffPixelRatio: 0.02,
+		maxDiffPixelRatio: 0.1,
 	})
 })
